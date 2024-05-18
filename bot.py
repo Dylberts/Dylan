15e8d91efa2ab8332ad4006967c1aa0780785b5d
Dylberts <dylan_e_davis@hotmail.com>
2024-05-17 17:55:09 -0700

    Create bot.py
    Main hub for the bot

---
+++ bot.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+def main():
+    client = commands.Bot(command_prefix="!")
+    
+    load_dotenv()
+
+    @client.event
+    async def on_ready():
+        print(f"{client.user.name} has connected to Discord.")
+
+    for folder in os.listdir("modules"):
+        if os.path.exists(os.path.join("modules", folder, "cog.py")):
+            client.load_extension(f"modules.{folder}.cog")
+
+    client.run(os.getenv("DISCORD_TOKEN")) ### remember to add token later
+
+if __name__ == '__main__':
+    main()
