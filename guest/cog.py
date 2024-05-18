937e4878e591e4f6417fc872318c380a159e967f
Dylberts <dylan_e_davis@hotmail.com>
2024-05-18 15:48:44 -0700

    Update cog.py

--- guest/cog.py
+++ guest/cog.py
@@ -8,9 +8,9 @@ class Guest(commands.Cog, name="Guest"):
         self.bot = bot
 
     @commands.command(name="guest", hidden=True)
-    async def remove_my_tole(self, ctx, role_id: int):
+    async def remove_my_tole(self, ctx, role_id: int="1240961142554234970u"):
         #role = discord.utils.get(ctx.guild.roles, id=role_id)
-        role = discord.utils.get(ctx.guild.roles, "1240961142554234970u"==role_id)
+        role = discord.utils.get(ctx.guild.roles, id=role_id)
         if role:
             await ctx.author.remove_roles(role)
             await ctx.message.delete() #silent
