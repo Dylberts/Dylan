780198696aac37d2b470469f9b99cc837930cc8e
Dylberts <dylan_e_davis@hotmail.com>
2024-05-19 11:03:50 -0700

    Update cog.py

--- RoleLock/cog.py
+++ RoleLock/cog.py
@@ -8,11 +8,11 @@ class RoleLock(commands.Cog, name="RoleLock"):
     @commands.Cog.listener(name="RoleLock")
     async def on_member_update(self, before, after):
         # Check if the specific role has been obtained
-        specific_role = discord.utils.get(after.roles, name="Age (13-17)")
+        specific_role = discord.utils.get(after.roles, name.id="1233366060477186048") # @Age (13-17)
         if specific_role:
             # Remove other roles specified
-            roles_to_remove = ["Age (18-25)", "Age (26-35)", "Age (36-48+)"]
-            roles_to_remove = [discord.utils.get(after.guild.roles, name=role_name) for role_name in roles_to_remove]
+            roles_to_remove = ["1233366502451712074", "1233366641572712520", "1233366931302776843"] # @Age (18-25), (26-35), (36-48+)
+            roles_to_remove = [discord.utils.get(after.guild.roles, name.id=role_name) for role_name in roles_to_remove]
             roles_to_remove = [role for role in roles_to_remove if role is not None]  # Filter out None values
 
             # If the user is attempting to obtain other roles, remove them
