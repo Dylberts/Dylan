0c977fa5dc30f7443ba98771319ac35ba73a6c3e
Dylberts <dylan_e_davis@hotmail.com>
2024-05-19 11:59:11 -0700

    Update cog.py

--- RoleLock/cog.py
+++ RoleLock/cog.py
@@ -4,20 +4,15 @@ import discord
 class RoleLock(commands.Cog, name="RoleLock"):
     def __init__(self, bot):
         self.bot = bot
+        self.locked_role_id = ("1233366060477186048")  # @Age (13-17) Role ID > Replace with the ID of the role you want to lock
+        self.blocked_role_ids = ("1233366502451712074", "1233366641572712520", "1233366931302776843")  # @Age (18-25), @Age (26-35), @Age (36-48+) Role ID > Replace with the ID of the role you want to block 
 
     @commands.Cog.listener(name="RoleLock")
     async def on_member_update(self, before, after):
-        # Check if the specific role has been obtained
-        specific_role = discord.utils.get(after.roles, name.id="1233366060477186048") # @Age (13-17)
-        if specific_role:
-            # Remove other roles specified
-            roles_to_remove = ["1233366502451712074", "1233366641572712520", "1233366931302776843"] # @Age (18-25), (26-35), (36-48+)
-            roles_to_remove = [discord.utils.get(after.guild.roles, name.id=role_name) for role_name in roles_to_remove]
-            roles_to_remove = [role for role in roles_to_remove if role is not None]  # Filter out None values
-
-            # If the user is attempting to obtain other roles, remove them
-            if any(role in after.roles for role in roles_to_add):
-                await after.remove_roles(*roles_to_add)
+        if self.locked_role_id in [role.id for role in after.roles]:
+            if self.blocked_role_ids in (role.id for role in after.roles):
+                await after.remove_roles(discord.Object(id=self.blocked_role_ids))
+                #await after.send("You cannot obtain this role while having the locked role.")
 
 def setup(bot):
     bot.add_cog(RoleLock(bot))
