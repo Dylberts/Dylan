22d5a734e451f2b785d970e8f7f0edefd3fa3c0f
Dylberts <dylan_e_davis@hotmail.com>
2024-05-20 03:12:44 -0700

    Update cog.py

--- RoleLock/cog.py
+++ RoleLock/cog.py
@@ -1,18 +1,6 @@
 from redbot.core import commands
 import discord
-
-##class RoleLock(commands.Cog, name="RoleLock"):
-    ##def __init__(self, bot):
-        ##self.bot = bot
-        ##self.locked_role_id = 1233366060477186048  # @Age (13-17) Role ID > Replace with the ID of the role you want to lock
-        ##self.blocked_role_ids = [1233366502451712074, 1233366641572712520, 1233366931302776843, 1233369491447091251]  # @Age (18-25), @Age (26-35), @Age (36-48+), @NSFW Role ID > Replace with the ID of the role you want to block 
-
-    ##@commands.Cog.listener()
-    ##async def on_member_update(self, before, after):
-        ##for role in after.roles:
-            ##if role.id in self.blocked_role_ids and self.locked_role_id in [role.id for role in after.roles]:
-                ##await after.remove_roles(role)
-                #await after.send("You cannot obtain this role while having the locked 
+ 
 class RoleLock(commands.Cog, name="RoleLock"):
     def __init__(self, bot):
         self.bot = bot
@@ -25,8 +13,7 @@ class RoleLock(commands.Cog, name="RoleLock"):
             1235798008126246983: [1235573502459904042, 1235798803425132624],
             # @Guild Elite: [Blocked Roles: @Guild Leader), @Guild Deputy]
             1235798803425132624: [1235573502459904042, 1235798008126246983],
-            # NOTE: You can add more entries for additional lock roles and their corresponding blocked roles
-            # TODO: Simplify nest dictionary so it's not a massive list
+            # TODO: Simplify nests so it's not a massive list (if possible?)
         }
 
     @commands.Cog.listener()
@@ -36,8 +23,6 @@ class RoleLock(commands.Cog, name="RoleLock"):
                 for role in after.roles:
                     if role.id in blocked_role_ids:
                         await after.remove_roles(role)
-                        embed = discord.Embed(title="Role Blocked", description=f"This is a Test", color=discord.Color.red())
-                        await ctx.send(embed=embed, delete_after=10)
-                        #await after.send("") 
+                         
 def setup(bot):
    bot.add_cog(RoleLock(bot))
