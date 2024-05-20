9af2f6cf1f12111a847705df79aec2a5a9a98bd0
Dylberts <dylan_e_davis@hotmail.com>
2024-05-20 04:51:13 -0700

    Update cog.py

--- RoleLock/cog.py
+++ RoleLock/cog.py
@@ -1,12 +1,10 @@
 from redbot.core import commands
 import discord
- 
+
 class RoleLock(commands.Cog, name="RoleLock"):
     def __init__(self, bot):
         self.bot = bot
         self.locked_roles = {
-            # @Age (13-17): [Blocked Roles: @Age (18-25), @Age (26-35), @Age (26-35), @Age (36-48+), @NSFW]
-            1233366060477186048: [1233366502451712074, 1233366641572712520, 1233366931302776843, 1233369491447091251],
             # @Guild Leader: [Blocked Roles: @Guild Deputy, @Guild Elite]
             1235573502459904042: [1235798008126246983, 1235798803425132624],
             # @Guild Deputy: [Blocked Roles: @Guild Leader, @Guild Elite]
@@ -32,7 +30,7 @@ class RoleLock(commands.Cog, name="RoleLock"):
             # All roles ID's nested are in the order provided
 
         }
-         
+     
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
         for locked_role_id, blocked_role_ids in self.locked_roles.items():
@@ -40,6 +38,16 @@ class RoleLock(commands.Cog, name="RoleLock"):
                 for role in after.roles:
                     if role.id in blocked_role_ids:
                         await after.remove_roles(role)
-                         
+                  
+        # New logic for Age roles
+        age_locked_role_id = 1233366060477186048
+        age_blocked_roles = [1233366502451712074, 1233366641572712520, 1233366931302776843, 1233369491447091251]
+        if age_locked_role_id in [role.id for role in after.roles]:
+            age_locked_role = discord.utils.get(after.roles, id=age_locked_role_id)
+            for role_id in age_blocked_roles:
+                role = discord.utils.get(after.guild.roles, id=role_id)
+                if role:
+                    await after.remove_roles(role)
+
 def setup(bot):
    bot.add_cog(RoleLock(bot))
