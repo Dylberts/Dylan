82198c76bd8442466db752a1efda0ea9abf855b0
Dylberts <dylan_e_davis@hotmail.com>
2024-05-20 21:58:20 -0700

    Update cog.py

--- RoleLock/cog.py
+++ RoleLock/cog.py
@@ -18,12 +18,10 @@ class RoleLock(commands.Cog, name="RoleLock"):
                     if role.id in blocked_role_ids:
                         await after.remove_roles(role)
 
- class RoleReplace(commands.Cog, name="RoleReplace"):
+class RoleReplace(commands.Cog, name="RoleReplace"):
     def __init__(self, bot):
         self.bot = bot
         self.locked_roles = {
-            # @Age (13-17): [Blocked Roles: @Age (18-25), @Age (26-35), @Age (36-48+), @NSFW]
-            1233366060477186048: [1233366502451712074, 1233366641572712520, 1233366931302776843, 1233369491447091251],
             # @Guild Leader: [Blocked Roles: @Guild Deputy, @Guild Elite]
             1235573502459904042: [1235798008126246983, 1235798803425132624],
             # @Guild Deputy: [Blocked Roles: @Guild Leader, @Guild Elite]
