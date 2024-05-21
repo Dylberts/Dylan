bfdfd88e8d70b294e1a129c951a000052681246e
Dylberts <dylan_e_davis@hotmail.com>
2024-05-20 20:49:04 -0700

    Update cog.py

--- RoleLock/cog.py
+++ RoleLock/cog.py
@@ -1,12 +1,27 @@
 from redbot.core import commands
 import discord
- 
+
 class RoleLock(commands.Cog, name="RoleLock"):
     def __init__(self, bot):
         self.bot = bot
         self.locked_roles = {
             # @Age (13-17): [Blocked Roles: @Age (18-25), @Age (26-35), @Age (26-35), @Age (36-48+), @NSFW]
-            1233366060477186048: [1233366502451712074, 1233366641572712520, 1233366931302776843, 1233369491447091251],
+            1233366060477186048: [1233366502451712074, 1233366641572712520, 1233366931302776843],
+            # All role ID's nested are in the order listed
+        }
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        for locked_role_id, blocked_role_ids in self.locked_roles.items():
+            if locked_role_id in [role.id for role in after.roles]:
+                for role in after.roles:
+                    if role.id in blocked_role_ids:
+                        await after.remove_roles(role)
+
+class RoleReplace(commands.Cog, name="RoleReplace"):
+    def __init__(self, bot):
+        self.bot = bot
+        self.locked_roles = {
             # @Guild Leader: [Blocked Roles: @Guild Deputy, @Guild Elite]
             1235573502459904042: [1235798008126246983, 1235798803425132624],
             # @Guild Deputy: [Blocked Roles: @Guild Leader, @Guild Elite]
@@ -27,19 +42,22 @@ class RoleLock(commands.Cog, name="RoleLock"):
             1233256301660864554: [1233254368963334185, 1233254790406869005, 1233255218817404938, 1233256062946246666, 1233256207112732812, 1233256478228484116, 1233256570247188510],
             # @S-207: [Blocked Roles: @S-201, @S-202, @S-203, @S-204, @S-205, @S-206, @S-208]
             1233256478228484116: [1233254368963334185, 1233254790406869005, 1233255218817404938, 1233256062946246666, 1233256207112732812, 1233256301660864554, 1233256570247188510],
-            # @S-207: [Blocked Roles: @S-201, @S-202, @S-203, @S-204, @S-205, @S-206, @S-207]
+            # @S-208: [Blocked Roles: @S-201, @S-202, @S-203, @S-204, @S-205, @S-206, @S-207]
             1233256570247188510: [1233254368963334185, 1233254790406869005, 1233255218817404938, 1233256062946246666, 1233256207112732812, 1233256301660864554, 1233256478228484116],
-            # All roles ID's nested are in the order provided
-
+            # All role ID's nested are in the order listed
         }
-         
+
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
         for locked_role_id, blocked_role_ids in self.locked_roles.items():
             if locked_role_id in [role.id for role in after.roles]:
                 for role in after.roles:
                     if role.id in blocked_role_ids:
-                        await after.remove_roles(role)
-                         
+                        locked_role = discord.utils.get(after.guild.roles, id=locked_role_id)
+                        blocked_role = discord.utils.get(after.guild.roles, id=role.id)
+                        await after.remove_roles(locked_role)
+                        await after.add_roles(blocked_role)
+
 def setup(bot):
-   bot.add_cog(RoleLock(bot))
+    bot.add_cog(RoleLock(bot))
+    bot.add_cog(RoleReplace(bot))
