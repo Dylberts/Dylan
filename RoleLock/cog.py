14550dec3b1c7f30a5e6cd42a49ff36b6d7b7c2b
Dylberts <dylan_e_davis@hotmail.com>
2024-05-20 21:48:41 -0700

    Update cog.py

--- RoleLock/cog.py
+++ RoleLock/cog.py
@@ -1,6 +1,5 @@
 from redbot.core import commands
 import discord
-import logging
 
 class RoleLock(commands.Cog, name="RoleLock"):
     def __init__(self, bot):
@@ -19,43 +18,50 @@ class RoleLock(commands.Cog, name="RoleLock"):
                     if role.id in blocked_role_ids:
                         await after.remove_roles(role)
 
-class RoleReplace(commands.Cog, name="RoleReplace"):
+ class RoleReplace(commands.Cog, name="RoleReplace"):
     def __init__(self, bot):
         self.bot = bot
-        self.role_replacements = {
-            # @New Role: @Old Role to replace
-            1235573502459904042: 1235798008126246983,  # @Guild Leader replaces @Guild Deputy
-            1235798008126246983: 1235798803425132624,  # @Guild Deputy replaces @Guild Elite
-            1235798803425132624: 1235573502459904042,  # @Guild Elite replaces @Guild Leader
-            1233254368963334185: 1233254790406869005,  # @S-201 replaces @S-202
-            1233254790406869005: 1233255218817404938,  # @S-202 replaces @S-203
-            1233255218817404938: 1233256062946246666,  # @S-203 replaces @S-204
-            1233256062946246666: 1233256207112732812,  # @S-204 replaces @S-205
-            1233256207112732812: 1233256301660864554,  # @S-205 replaces @S-206
-            1233256301660864554: 1233256478228484116,  # @S-206 replaces @S-207
-            1233256478228484116: 1233256570247188510,  # @S-207 replaces @S-208
-            1233256570247188510: 1233254368963334185,  # @S-208 replaces @S-201
+        self.locked_roles = {
+            # @Age (13-17): [Blocked Roles: @Age (18-25), @Age (26-35), @Age (36-48+), @NSFW]
+            1233366060477186048: [1233366502451712074, 1233366641572712520, 1233366931302776843, 1233369491447091251],
+            # @Guild Leader: [Blocked Roles: @Guild Deputy, @Guild Elite]
+            1235573502459904042: [1235798008126246983, 1235798803425132624],
+            # @Guild Deputy: [Blocked Roles: @Guild Leader, @Guild Elite]
+            1235798008126246983: [1235573502459904042, 1235798803425132624],
+            # @Guild Elite: [Blocked Roles: @Guild Leader, @Guild Deputy]
+            1235798803425132624: [1235573502459904042, 1235798008126246983],
+            # @S-201: [Blocked Roles: @S-202, @S-203, @S-204, @S-205, @S-206, @S-207, @S-208]
+            1233254368963334185: [1233254790406869005, 1233255218817404938, 1233256062946246666, 1233256207112732812, 1233256301660864554, 1233256478228484116, 1233256570247188510],
+            # @S-202: [Blocked Roles: @S-201, @S-203, @S-204, @S-205, @S-206, @S-207, @S-208]
+            1233254790406869005: [1233254368963334185, 1233255218817404938, 1233256062946246666, 1233256207112732812, 1233256301660864554, 1233256478228484116, 1233256570247188510],
+            # @S-203: [Blocked Roles: @S-201, @S-202, @S-204, @S-205, @S-206, @S-207, @S-208]
+            1233255218817404938: [1233254368963334185, 1233254790406869005, 1233256062946246666, 1233256207112732812, 1233256301660864554, 1233256478228484116, 1233256570247188510],
+            # @S-204: [Blocked Roles: @S-201, @S-202, @S-203, @S-205, @S-206, @S-207, @S-208]
+            1233256062946246666: [1233254368963334185, 1233254790406869005, 1233255218817404938, 1233256207112732812, 1233256301660864554, 1233256478228484116, 1233256570247188510],
+            # @S-205: [Blocked Roles: @S-201, @S-202, @S-203, @S-204, @S-206, @S-207, @S-208]
+            1233256207112732812: [1233254368963334185, 1233254790406869005, 1233255218817404938, 1233256062946246666, 1233256301660864554, 1233256478228484116, 1233256570247188510],
+            # @S-206: [Blocked Roles: @S-201, @S-202, @S-203, @S-204, @S-205, @S-207, @S-208]
+            1233256301660864554: [1233254368963334185, 1233254790406869005, 1233255218817404938, 1233256062946246666, 1233256207112732812, 1233256478228484116, 1233256570247188510],
+            # @S-207: [Blocked Roles: @S-201, @S-202, @S-203, @S-204, @S-205, @S-206, @S-208]
+            1233256478228484116: [1233254368963334185, 1233254790406869005, 1233255218817404938, 1233256062946246666, 1233256207112732812, 1233256301660864554, 1233256570247188510],
+            # @S-208: [Blocked Roles: @S-201, @S-202, @S-203, @S-204, @S-205, @S-206, @S-207]
+            1233256570247188510: [1233254368963334185, 1233254790406869005, 1233255218817404938, 1233256062946246666, 1233256207112732812, 1233256301660864554, 1233256478228484116],
+            # All roles ID's nested are in the order provided
         }
-        
+
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
-        logging.info("on_member_update triggered")
-        before_roles = set(before.roles)
-        after_roles = set(after.roles)
-
-        newly_added_roles = after_roles - before_roles
-
-        for new_role in newly_added_roles:
-            if new_role.id in self.role_replacements:
-                logging.info(f"New role detected: {new_role.name} ({new_role.id})")
-                old_role_id = self.role_replacements[new_role.id]
-                old_role = discord.utils.get(after.guild.roles, id=old_role_id)
-
-                if old_role in after_roles:
-                    logging.info(f"Removing old role: {old_role.name} ({old_role.id})")
-                    await after.remove_roles(old_role)
-                    logging.info(f"Adding new role: {new_role.name} ({new_role.id})")
-                    await after.add_roles(new_role)
+        for locked_role_id, blocked_role_ids in self.locked_roles.items():
+            # Check if the member has a locked role
+            if locked_role_id in [role.id for role in before.roles]:
+                for role in after.roles:
+                    # If the member gets a blocked role, replace the locked role
+                    if role.id in blocked_role_ids:
+                        locked_role = discord.utils.get(after.guild.roles, id=locked_role_id)
+                        if locked_role:
+                            await after.remove_roles(locked_role)
+                        await after.add_roles(role)
+                        break
                     
 def setup(bot):
     bot.add_cog(RoleLock(bot))
