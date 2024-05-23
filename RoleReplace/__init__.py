945c4543c0a0dd09a69b739e93ac2ccba7e000ce
Dylberts <dylan_e_davis@hotmail.com>
2024-05-23 01:01:08 -0700

    Update __init__.py

--- RoleReplace/__init__.py
+++ RoleReplace/__init__.py
@@ -1,5 +1,10 @@
 from .cog import RoleReplace
 
-
+cog = bot.get_cog("RoleTools")
+if cog:
+    print("RoleTools cog is loaded.")
+else:
+    print("RoleTools cog is not loaded.")
+    
 async def setup(bot):
     await bot.add_cog(RoleReplace(bot))
