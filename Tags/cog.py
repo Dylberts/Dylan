2dd34ccc1233319d6997d429be764fdbadc64a87
Dylberts <dylan_e_davis@hotmail.com>
2024-06-27 03:07:15 -0700

    Update cog.py

--- Tags/cog.py
+++ Tags/cog.py
@@ -3,25 +3,24 @@ from redbot.core import commands
 from redbot.core.bot import Red
 
 class Tags(commands.Cog):
-    """A simple cog for Red Bot with a button link"""
+    """Simple tag commands will be created here"""
 
     def __init__(self, bot: Red):
         self.bot = bot
 
     @commands.command()
     async def tiermaker(self, ctx: commands.Context):
-        """Responds with a friendly message and a button link"""
+        """Responds with an embed image and a button link"""
         button = discord.ui.Button(
             label="Create Tiermaker", 
-            url="https://tiermaker.com/create/isekai-slow-life-fellows-16506986",
-            style=discord.ButtonStyle.primary  # This sets the button color to blue
+            url="https://tiermaker.com/create/isekai-slow-life-fellows-16506986"
         )
 
         view = discord.ui.View()
         view.add_item(button)
 
-        embed = discord.Embed()
-        embed.set_image(url="https://imgur.com/a/QjFJMIU")  # Replace with your image URL
+        embed = discord.Embed(color=discord.Color(0x6EDFBA))
+        embed.set_image(url="https://imgur.com/m2V37I9")  # Replace with your image URL
 
         await ctx.send(embed=embed, view=view)
 
