2d919a783257d05a70d8fdef8daf62eaacae0ae8
Dylberts <dylan_e_davis@hotmail.com>
2024-06-28 13:03:39 -0700

    Update cog.py

--- DailyQ/cog.py
+++ DailyQ/cog.py
@@ -171,7 +171,7 @@ class DailyQ(commands.Cog):
         """Generate a 'Would You Rather' question using the 'would-you-rather-api'."""
         async with aiohttp.ClientSession() as session:
             try:
-                async with session.get('https://would-you-rather.willbeaumont.repl.co') as response:
+                async with session.get('https://either.io/api/questions') as response:
                     if response.status == 200:
                         data = await response.json()
                         question = data.get("question")
