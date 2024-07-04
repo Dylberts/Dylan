3cbf17a05de553935d59873ea9e2aa223b23fef9
Dylberts <dylan_e_davis@hotmail.com>
2024-07-04 02:24:32 -0700

    Create madlibs.py

---
+++ MadLib/madlibs.py
@@ -0,0 +1,9 @@
+# Stores all the MadLibs
+MADLIBS = [
+    {
+        "title": "A Day at the Zoo",
+        "template": "Today I went to the zoo. I saw a(n) {adjective} {noun} jumping up and down in its tree. It {verb} {adverb} through the large tunnel that led to its {adjective} {noun}. I got some peanuts and passed them through the cage to a gigantic gray {noun} towering above my head. Feeding that animal made me hungry. I went to get a {adjective} scoop of ice cream. It filled my stomach. Afterwards, I had to {verb} {adverb} to catch our bus. When I got home, I {verb} my mom for a {adjective} day at the zoo.",
+        "placeholders": ["adjective", "noun", "verb", "adverb", "adjective", "noun", "noun", "adjective", "verb", "adverb", "verb", "adjective"]
+    },
+    # Add more MadLibs templates as needed
+]
