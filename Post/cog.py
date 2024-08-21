5a38b02912f0727f2f44be5fd2c129c40a5ae491
Dylberts <dylan_e_davis@hotmail.com>
2024-08-21 10:54:38 -0700

    Update cog.py

--- Post/cog.py
+++ Post/cog.py
@@ -7,6 +7,7 @@ class Post(commands.Cog):
         self.config = Config.get_conf(self, identifier=1234567890)
         default_guild = {
             "forum_channel": None,
+            "thread_channel": None,
         }
         self.config.register_guild(**default_guild)
 
@@ -22,6 +23,12 @@ class Post(commands.Cog):
         """Set the forum channel"""
         await self.config.guild(ctx.guild).forum_channel.set(channel.id)
         await ctx.send(f"Forum channel set to {channel.mention}")
+
+    @post.command()
+    async def setthread(self, ctx, channel: discord.TextChannel):
+        """Set the thread channel"""
+        await self.config.guild(ctx.guild).thread_channel.set(channel.id)
+        await ctx.send(f"Thread channel set to {channel.mention}")
 
     @post.command()
     async def forum(self, ctx, title: str, *, description: str):
@@ -40,11 +47,38 @@ class Post(commands.Cog):
             await ctx.send("Description cannot be empty.")
             return
 
-        # Create the thread with the provided title and description
+        # Create the thread with the provided title and description in the forum channel
         thread = await forum_channel.create_thread(name=title, content=description, auto_archive_duration=1440)
         embed = discord.Embed(
             title="Forum Thread Created",
             description=f"[Click here to view it](https://discord.com/channels/{ctx.guild.id}/{forum_channel.id}/{thread.id})",
+            color=0x6EDFBA
+        )
+        await ctx.send(embed=embed)
+
+    @post.command()
+    async def thread(self, ctx, title: str, *, message: str):
+        """Create a new thread in the specified thread channel"""
+        channel_id = await self.config.guild(ctx.guild).thread_channel()
+        if not channel_id:
+            await ctx.send("Thread channel not set. Use `post setthread` to set it.")
+            return
+
+        thread_channel = ctx.guild.get_channel(channel_id)
+        if not thread_channel:
+            await ctx.send("Thread channel not found.")
+            return
+
+        if not message:
+            await ctx.send("Message cannot be empty.")
+            return
+
+        # Send the initial message and create a thread in the specified text channel
+        msg = await thread_channel.send(message)
+        thread = await msg.create_thread(name=title, auto_archive_duration=1440)
+        embed = discord.Embed(
+            title="Thread Created",
+            description=f"[Click here to view it](https://discord.com/channels/{ctx.guild.id}/{thread_channel.id}/{thread.id})",
             color=0x6EDFBA
         )
         await ctx.send(embed=embed)
